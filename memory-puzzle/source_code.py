8ed7b26735fd30dbf0d393a14616c79823a8f42c
Raunak Srivastava <153118278+OGKlausMikaelson@users.noreply.github.com>
2024-05-14 15:04:09 +0000

    source-code

--- memory-puzzle/source_code.py
+++ memory-puzzle/source_code.py
@@ -1,7 +1,64 @@
 import random, pygame, sys 
 from pygame.locals import *
 
-FPS =30
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 480
+FPS =30 # frames per second, general speed of the program
+WINDOWWIDTH = 640 # size of windows width in pixels
+WINDOWHEIGHT = 480 # size of windows height in pixels
+REVEALSPEED = 8 # SPEED OF BOXES REVEALING AND HIDING
+BOXSIZE = 40 # size of box height and width in pixels
+GAPSIZE = 10 # size of gaps btw boxes in pixels
+BOARDWIDTH = 10 # number of columns of icons
+BOARDHEIGHT = 7 # number of rows of icons
+assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board need to have an even number of boxes for pairs to match.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+#           R    G    B
+GRAY     =(100 ,100, 100)
+NAVYBLUE =( 60 , 60, 100)
+WHITE    =(255 ,255, 255)
+RED      =(255 ,  0,   0)
+GREEN    =(  0 ,255,   0)
+BLUE     =(  0 ,  0, 255)
+YELLOW   =(255 ,255,   0)
+ORANGE   =(255 ,128,   0)
+PURPLE   =(255 ,  0, 255)
+CYAN     =(  0 ,255, 255)
+
+BGCOLOR        = NAVYBLUE
+LIGHTBGCOLOR   = GRAY
+BOXCOLOR       = WHITE
+HIGHLIGHTCOLOR = BLUE
+
+DONUT   = 'donut'
+SQUARE  = 'square'
+DIAMOND = 'diamond'
+LINES   = 'lines'
+OVAL    = 'oval'
+
+ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, BLUE, CYAN)
+ALLSHAPES = (DONUT,SQUARE,DIAMOND,LIGHTBGCOLOR,OVAL)
+assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDHEIGHT * BOARDWIDTH, "Board is too big for the number of shapes/colors defined."
+
+def main() :
+    global FPSCLOCK, DISPLAYSURF
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode(WINDOWWIDTH, WINDOWHEIGHT)
+
+    mousex = 0  # use to store x coordinate of mouse event
+    mousey = 0  # use to store y coordinate of mouse event
+    pygame.display.set_caption('Memory Game')
+
+    mainBoard = getRandomizedBoard()
+    revealedBoxes = generateRevealedBoxesData(False)
+
+    firstSelection = None # stores the (x,y) of the first box revealed
+
+    DISPLAYSURF.fill(BGCOLOR) #drawing the board
+    drawBoard(mainBoard,revealedBoxes)
+
+    for event in pygame.event.get(): # event handling loop
+        if event.type == 'QUIT' 
+
 
